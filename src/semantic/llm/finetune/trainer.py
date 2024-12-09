83a57d4f87e9c780e80edd695b42107c985af607
dawnik17 <135340243+dawnik17@users.noreply.github.com>
2024-12-09 16:57:30 +0530

    Update trainer.py

--- src/semantic/llm/finetune/trainer.py
+++ src/semantic/llm/finetune/trainer.py
@@ -2,25 +2,6 @@ from sentence_transformers import SentenceTransformer, models
 from transformers.trainer import *
 
 
-def save_ckpt_for_sentence_transformers(
-    ckpt_dir, pooling_mode: str = "cls", normlized: bool = True
-):
-    word_embedding_model = models.Transformer(ckpt_dir)
-    pooling_model = models.Pooling(
-        word_embedding_model.get_word_embedding_dimension(), pooling_mode=pooling_mode
-    )
-    if normlized:
-        normlize_layer = models.Normalize()
-        model = SentenceTransformer(
-            modules=[word_embedding_model, pooling_model, normlize_layer], device="cpu"
-        )
-    else:
-        model = SentenceTransformer(
-            modules=[word_embedding_model, pooling_model], device="cpu"
-        )
-    model.save(ckpt_dir)
-
-
 class BiTrainer(Trainer):
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
         output_dir = output_dir if output_dir is not None else self.args.output_dir
@@ -39,14 +20,6 @@ class BiTrainer(Trainer):
             self.tokenizer.save_pretrained(output_dir)
 
         torch.save(self.args, os.path.join(output_dir, "training_args.bin"))
-
-        # save the checkpoint for sentence-transformers library
-        if self.is_world_process_zero():
-            save_ckpt_for_sentence_transformers(
-                output_dir,
-                pooling_mode=self.args.sentence_pooling_method,
-                normlized=self.args.normlized,
-            )
 
     def compute_loss(self, model, inputs, return_outputs=False):
         """
